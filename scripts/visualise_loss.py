c1427fa5553b72ca0fda1f4f21686caa1fb793c4
jimxing <xyf.jim@gmail.com>
2017-04-26 09:30:07 -0400

    change to single domain visualisation loss

--- scripts/visualise_loss.py
+++ scripts/visualise_loss.py
@@ -1,36 +1,23 @@
-import numpy as np
+import numpy as np 
 import cPickle
-import matplotlib
-# matplotlib.use('TkAgg')  # for visualisation + writing
-# matplotlib.use('WX')  # for visualisation + writing
-# matplotlib.use('Agg')  # for just writing
 import matplotlib.pyplot as plt
 import sys
 
 def main():
 	loss_history_file = sys.argv[1]
-	loss_history_total = np.genfromtxt(loss_history_file, delimiter = ' ') # since now each column is for one domain
+	loss_history = np.genfromtxt(loss_history_file, delimiter = ' ')
+	avg_period = 100
 
-	# k = int(sys.argv[2])
-	for k in range(0, 89):
-		loss_history = loss_history_total[:, k]
-		avg_period = 10
-
-		print "minimum loss:", np.min(loss_history)
-		print "maximum loss:", np.max(loss_history)
-		loss_history_to_plot = np.zeros(len(loss_history)/avg_period)
-		for i in range(0, len(loss_history_to_plot)):
-			loss_history_to_plot[i] = np.average(loss_history[i*avg_period:(i+1)*avg_period])
-		plt.plot(loss_history_to_plot)
-		plt.ylabel('loss')
-		plt.xlabel('iterations')
-		plt.title(loss_history_file[:loss_history_file.find('.')])
-		plt.show(block=False)
-		duration = 2    # [sec]
-		plt.pause(duration)
-		plt.clf()
-    	plt.close()
-	    # plt.savefig('loss_plot.png')
+	print "minimum loss:", np.min(loss_history)
+	print "maximum loss:", np.max(loss_history)
+	loss_history_to_plot = np.zeros(len(loss_history)/avg_period)
+	for i in range(0, len(loss_history_to_plot)):
+		loss_history_to_plot[i] = np.average(loss_history[i*avg_period:(i+1)*avg_period])
+	plt.plot(loss_history_to_plot)
+	plt.ylabel('loss')
+	plt.xlabel('iterations')
+	plt.title(loss_history_file[:loss_history_file.find('.')])
+	plt.show()
 
 	return
 
