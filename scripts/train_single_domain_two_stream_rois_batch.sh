efdc4eccad31829e32dcaf9e9b2065c2a417dab3
jimxing <xyf.jim@gmail.com>
2017-05-13 02:47:03 -0400

    update train script

--- scripts/train_single_domain_two_stream_rois_batch.sh
+++ scripts/train_single_domain_two_stream_rois_batch.sh
@@ -7,7 +7,7 @@ fi
 VIDEOS_FOLDER=$1
 CAFFE_MODEL=nets/models/weights_init/tracker_init.caffemodel
 TRAIN_PROTO=nets/tracker_new_train.prototxt
-SOLVER=nets/solver_temp/solver_temp_GOTURN_MDNET_TWO_STREAM_ROIS_BATCH_SINGLE.prototxt
+SOLVER=nets/solver_temp/solver_temp_GOTURN_MDNET_TWO_STREAM_ROIS_BATCH_SINGLE_NO_POOL.prototxt
 LAMBDA_SHIFT=5
 LAMBDA_SCALE=15
 MIN_SCALE=-0.4
@@ -15,4 +15,4 @@ MAX_SCALE=0.4
 GPU_ID=0
 RANDOM_SEED=800
 
-./build/train_single_domain_no_middle_batch $VIDEOS_FOLDER $CAFFE_MODEL $TRAIN_PROTO $SOLVER $LAMBDA_SHIFT $LAMBDA_SCALE $MIN_SCALE $MAX_SCALE $GPU_ID $RANDOM_SEED
+./build/train_single_domain_no_middle_batch_no_pool $VIDEOS_FOLDER $CAFFE_MODEL $TRAIN_PROTO $SOLVER $LAMBDA_SHIFT $LAMBDA_SCALE $MIN_SCALE $MAX_SCALE $GPU_ID $RANDOM_SEED
