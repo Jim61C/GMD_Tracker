035a8bc2c1fa9bb008b4766c1a39097547dadbb5
jimxing <xyf.jim@gmail.com>
2017-11-26 04:48:19 -0500

    ENH: remove logging in multi domain training

--- src/network/regressor_train.cpp
+++ src/network/regressor_train.cpp
@@ -20,8 +20,8 @@ using caffe::LayerParameter;
 // #define DEBUG_OBSERVE_PREDICTION
 // #define DEBUG_CROSS_FRAME_OHEM
 // #define DEBUG_MDNET_TRAIN
-#define DEBUG_TIME
-#define DEBUG_SET_DIFF_MULTI_DOMAIN
+// #define DEBUG_TIME
+// #define DEBUG_SET_DIFF_MULTI_DOMAIN
 
 RegressorTrain::RegressorTrain(const std::string& deploy_proto,
                                const std::string& caffe_model,
