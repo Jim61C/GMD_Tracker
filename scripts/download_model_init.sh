a7ac9dec0faf9676a31af38386e8e41331db6dac
jimxing <xyf.jim@gmail.com>
2017-12-05 02:19:28 -0500

    ENH: update download pretrained and weights init scripts

--- scripts/download_model_init.sh
+++ scripts/download_model_init.sh
@@ -4,3 +4,8 @@ mkdir -p nets/models/weights_init
 cd nets/models/weights_init
 wget http://cs.stanford.edu/people/davheld/public/GOTURN/weights_init/tracker_init.caffemodel
 cd ../../../
+
+
+cd nets/models/weights_init
+wget http://www.jimxingyf.com/static/models/VGG_CNN_M.caffemodel
+cd ../../../
