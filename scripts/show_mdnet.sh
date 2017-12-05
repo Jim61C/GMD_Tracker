e4109e0b64bd1bda2b92438626bb889a813d8f09
jimxing <xyf.jim@gmail.com>
2017-12-05 00:54:42 -0500

    NEW: add mdnet solver prototxt tracking + use the correct train architecture + update show_mdnet.sh accordingly

--- scripts/show_mdnet.sh
+++ scripts/show_mdnet.sh
@@ -20,13 +20,13 @@ SHOW_RESULT=1
 
 VIDEOS_FOLDER=$1
 DEPLOY=nets/mdnet_finetune.prototxt
-CAFFE_MODEL=/home/jimxing/proj/MDNet_C++/nets/models/mdnet_OTB-VOT2014_from_matlab.caffemodel
+CAFFE_MODEL=nets/solverstate/MDNET/caffenet_train_MDNET_OTB-VOT2014.caffemodel.h5
 SOLVER_FILE=nets/solver_temp/solver_temp_mdnet_finetune.prototxt
 LAMBDA_SHIFT=5
 LAMBDA_SCALE=15
 MIN_SCALE=-0.4
 MAX_SCALE=0.4
-OUTPUT_FOLDER=nets/tracker_output/mdnet_C++
+OUTPUT_FOLDER=nets/tracker_output/mdnet_C++_OTB-VOT2014
 
 build/show_mdnet $DEPLOY $CAFFE_MODEL $SOLVER_FILE $VIDEOS_FOLDER \
 $LAMBDA_SHIFT $LAMBDA_SCALE $MIN_SCALE $MAX_SCALE $GPU_ID $START_VIDEO_NUM $PAUSE_VAL $OUTPUT_FOLDER $SHOW_RESULT
