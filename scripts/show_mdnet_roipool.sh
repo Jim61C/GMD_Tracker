1b122e4dfb609f0ebee5e226f14b9c2f0e2fa2f4
jimxing <xyf.jim@gmail.com>
2017-12-05 01:41:28 -0500

    ENH: add solver temp prototxt and update show_mdnet_roipool accordingly

--- scripts/show_mdnet_roipool.sh
+++ scripts/show_mdnet_roipool.sh
@@ -20,13 +20,13 @@ SHOW_RESULT=1
 
 VIDEOS_FOLDER=$1
 DEPLOY=nets/mdnet_roipool_finetune.prototxt
-CAFFE_MODEL=nets/solverstate/mdnet2/caffenet_train_iter_400000.caffemodel.h5
+CAFFE_MODEL=nets/solverstate/MDNET_ROIPOOL/caffenet_train_MDNET_ROIPOOL_OTB-VOT2014.caffemodel.h5
 SOLVER_FILE=nets/solver_temp/solver_temp_mdnet_roipool_finetune.prototxt
 LAMBDA_SHIFT=5
 LAMBDA_SCALE=15
 MIN_SCALE=-0.4
 MAX_SCALE=0.4
-OUTPUT_FOLDER=nets/tracker_output/mdnet_C++_ROIPOOL
+OUTPUT_FOLDER=nets/tracker_output/mdnet_C++_ROIPOOL_VOT2014
 
 build/show_mdnet_roipool $DEPLOY $CAFFE_MODEL $SOLVER_FILE $VIDEOS_FOLDER \
 $LAMBDA_SHIFT $LAMBDA_SCALE $MIN_SCALE $MAX_SCALE $GPU_ID $START_VIDEO_NUM $PAUSE_VAL $OUTPUT_FOLDER $SHOW_RESULT
