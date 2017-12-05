e2e57815464e0e0db5c1a2689028c782ff255163
jimxing <xyf.jim@gmail.com>
2017-12-05 02:19:28 -0500

    ENH: update download pretrained and weights init scripts [cherry-picked]

--- scripts/download_trained_model.sh
+++ scripts/download_trained_model.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-mkdir -p nets/models/pretrained_model 
-cd nets/models/pretrained_model
-wget http://cs.stanford.edu/people/davheld/public/GOTURN/trained_model/tracker.caffemodel
+mkdir -p nets/solverstate/GOTURN_MDNET
+cd nets/solverstate/GOTURN_MDNET
+wget http://www.jimxingyf.com/static/models/caffenet_train_GOTURN_MDNET_IMAGENET.caffemodel.h5
+cd ../../../
+
+
+mkdir -p nets/solverstate/MDNET
+cd nets/solverstate/MDNET
+wget http://www.jimxingyf.com/static/models/caffenet_train_MDNET_OTB-VOT2014.caffemodel.h5
+wget http://www.jimxingyf.com/static/models/caffenet_train_MDNET_OTB-VOT2015.caffemodel
+wget http://www.jimxingyf.com/static/models/caffenet_train_MDNET_OTB-VOT2016.caffemodel
+cd ../../../
+
+
+mkdir -p nets/solverstate/MDNET_ROIPOOL
+cd nets/solverstate/MDNET_ROIPOOL
+wget http://www.jimxingyf.com/static/models/caffenet_train_MDNET_ROIPOOL_OTB-VOT2014.caffemodel.h5
+cd ../../../
+
+
+mkdir -p nets/solverstate/MDNET_ROIPOOL_NOPOOL
+cd nets/solverstate/MDNET_ROIPOOL_NOPOOL
+wget http://www.jimxingyf.com/static/models/caffenet_train_MDNET_ROIPOOL_NOPOOL_OTB-VOT2014.caffemodel.h5
 cd ../../../
