049efc82f67a63d2d7ba12f103c6ab0ac57752dd
yxing1_cube3 <xyf.jim@gmail.com>
2017-12-10 19:03:57 -0500

    ENH: fix bbox regressor feature extraction layer from conv3 + reduce roipool input size

--- src/network/regressor.cpp
+++ src/network/regressor.cpp
@@ -346,12 +346,12 @@ void Regressor::GetBBoxConvFeatures(const cv::Mat& image_curr,
 
       SetCandidates(this_candidate_images);
       const vector<string> & layer_names = net_->layer_names();
-      int layer_relu3_idx = FindLayerIndexByName(layer_names, "roi_pool3");
+      int layer_relu3_idx = FindLayerIndexByName(layer_names, "relu3");
       net_->ForwardTo(layer_relu3_idx);
 
-      // get the roi_pool3 data blob features, 3*3*512
+      // get the conv3 data blob features, 3*3*512
       vector<vector<float> > output_features;
-      WrapOutputBlob("roi_pool3", &output_features);
+      WrapOutputBlob("conv3", &output_features);
       features.insert(features.end(), output_features.begin(), output_features.begin() + output_features.size());
     }
 }
